4c1fb31e980ab18e7145ea94dcf3c4b1907df29b
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-06-25 12:17:37 +0000

    Additions to GPS

--- src/Sensors/GPS/gps.h
+++ src/Sensors/GPS/gps.h
@@ -2,6 +2,7 @@
 #define GPS_I
 
 #include<TinyGPS++.h>
+#include<SoftwareSerial.h>
 
 #define RX_PIN 0
 #define TX_PIN 0
@@ -9,14 +10,24 @@
 class GPS {
     private:
         TinyGPSPlus gps;
-        
+        static const uint32_t GPSBaud = 9600;
+    
     public:
+        //Constructor
+        GPS();
+
+        //Function to get latitude from GPS
         double getLatitude();
 
+        //Function to get longitude from GPS
         double getLongitude();
 
+        //Function to get approxiamate altitude from GPS
+        //Returns meters
         double getapproxAltitude();
 
+        //Function to return array of current time from GPS
+        //{Hour,Min,Sec}
         double *getTime();
 
 };
