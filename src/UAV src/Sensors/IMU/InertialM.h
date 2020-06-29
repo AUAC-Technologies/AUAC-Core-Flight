7bf6023a0e79f4d9dcfabb9a63c6afe97707942b
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-06-29 20:19:37 +0000

    Changes

--- src/UAV src/Sensors/IMU/InertialM.h
+++ src/UAV src/Sensors/IMU/InertialM.h
@@ -2,16 +2,29 @@
 #define InertialM
 
 //#include"../../../lib/i2cdevlib-master/Arduino/MPU6050/MPU6050.h"
+//#include"MPU6050.h"
+//#include"MPU6050_6Axis_MotionApps20.h"
 
 class IMU {
+    private:
+        void setOffsets();
+
+        void dmpInitError();
+
+        void MPUMath();
+
     public:
+        IMU();
+
+        void MPU6050Connect();
+
         double getPitchAxis();
 
         double getYawAxis();
 
         double getRollAxis();
 
-
+        void GetDMP();
 };
 
 #endif
