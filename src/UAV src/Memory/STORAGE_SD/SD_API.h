fbf04ceeff482aa2d275dfb488c9ba2389c2ff6e
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-08-11 12:23:30 +0000

    additions

---
+++ src/UAV src/Memory/STORAGE_SD/SD_API.h
@@ -0,0 +1,24 @@
+#ifndef SD
+#define SD
+
+#include"../../cores/core-libs/SPI/src/SPI.h"
+#include"../../lib/SD-master/SD.h"
+#include"../../utility/definitions.h"
+
+class SD_API {
+    private:
+        NO_RETURN createFile();
+
+        NO_RETURN deleteFile();
+        
+    public:
+
+
+
+
+
+
+
+};
+
+#endif
