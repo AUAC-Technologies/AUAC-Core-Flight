ada72035e42a6fd6a8e80b6a3606b5fde7ec3905
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-08-14 17:45:42 +0000

    add

---
+++ app/AUAC-APP/AUAC-APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AUAC-APP
+//
+//  Created by Nyameaama Gambrah on 8/14/20.
+//  Copyright © 2020 Nyameaama Gambrah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
